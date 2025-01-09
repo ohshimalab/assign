feedec2def885c7bdf3f1666e80535a13cd86f10
longpham28 <44454827+longpham28@users.noreply.github.com>
2025-01-09 11:29:22 +0900

    feat: add input page(home page)

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,89 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
+"use client";
+import { Button } from "@nextui-org/button";
+import { Input } from "@nextui-org/react";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+export interface UserInput {
+  id: string;
+  name: string;
+}
 
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+type InputRowProps = {
+  userInput: UserInput;
+  onChange: (value: string) => void;
+  onDelete: () => void;
+};
+
+const InputRow: React.FC<InputRowProps> = ({
+  userInput,
+  onChange,
+  onDelete,
+}) => {
+  return (
+    <div className="w-full flex gap-4 items-center">
+      <Input
+        placeholder="名前を入力"
+        size="lg"
+        value={userInput.name}
+        onChange={(e) => onChange(e.target.value)}
+      />
+      <Button color="danger" size="lg" onPress={() => onDelete()}>
+        削除
+      </Button>
+    </div>
+  );
+};
 
 export default function Home() {
+  const [inputs, setInputs] = useState<UserInput[]>([]);
+
+  const onInputChange = (id: string, value: string) => {
+    setInputs((prevInputs) => {
+      return prevInputs.map((input) => {
+        if (input.id === id) {
+          return { ...input, name: value };
+        } else {
+          return input;
+        }
+      });
+    });
+  };
+  const onAddInput = () => {
+    setInputs((prevInputs) => {
+      return [
+        ...prevInputs,
+        {
+          id: uuidv4(),
+          name: "",
+        },
+      ];
+    });
+  };
+  const onDeleteInput = (id: string) => {
+    return setInputs((prevInputs) => {
+      return prevInputs.filter((input) => input.id !== id);
+    });
+  };
+
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
-        <br />
-        <span className={title()}>
-          websites regardless of your design experience.
-        </span>
-        <div className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </div>
-      </div>
-
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
-
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
-      </div>
+      <Button fullWidth color="primary" size="lg">
+        実行
+      </Button>
+      <Button fullWidth color="secondary" size="lg" variant="bordered">
+        共有
+      </Button>
+      {inputs.map((input) => (
+        <InputRow
+          key={input.id}
+          userInput={input}
+          onChange={(value) => onInputChange(input.id, value)}
+          onDelete={() => onDeleteInput(input.id)}
+        />
+      ))}
+      <Button fullWidth color="success" size="lg" onPress={() => onAddInput()}>
+        追加
+      </Button>
     </section>
   );
 }
